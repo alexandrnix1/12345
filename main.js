fa9ef82b4240a949cdd4faa4902d15688e326040
4aek-dotru <andrey12112005@yandex.ru>
2026-03-31 16:20:41 +0300

    initial commit

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('button[data-group]').forEach(button => {
-    button.onclick = event => {
-        document.querySelectorAll('.page').forEach(Element =>{
-            console.log(Element)
-            Element.classList.toggle('show');
-        })
-    }
-})
+document.querySelectorAll('[data-target]').forEach(button => {
+  button.addEventListener('click', () => {
+    const group = button.dataset.group;
+    const targetId = button.dataset.target;
+
+    document.querySelectorAll(`.content-page[data-group="${group}"]`).forEach(page => {
+      page.style.display = 'none';
+    });
+
+    document.getElementById(targetId).style.display = 'block';
+  });
+});
